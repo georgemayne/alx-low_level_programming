942306f0bdffcabe334ffe2e78167c2ef7ec339c
root <georgeegyirmayne07@gmail.com>
2023-03-16 18:37:30 -0700

    Corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - print quote
- *
- * Description: prints Dors's quote
+ * main - Entry point
  *
  * Return: Always 1 (Success)
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	return (1);
 }

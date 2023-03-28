22459828113ff840c3a1e5378dc09153acfe11a7
root <georgeegyirmayne07@gmail.com>
2023-03-27 17:43:21 -0700

    C - Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -14,4 +17,4 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _putchar(char c);
 
-#endif /* MAIN_H*/
+#endif

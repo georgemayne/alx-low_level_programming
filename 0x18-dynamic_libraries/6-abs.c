14b32d85059af65027452dd9f144caa57d6ed14d
georgemayne <georgeegyirmayne07@gmail.com>
2023-08-21 19:27:09 -0700

    C - Dynamic libraries

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @a: input number as an integer.
+ * Return: absolute
+ */
+
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (-a);
+	}
+}
